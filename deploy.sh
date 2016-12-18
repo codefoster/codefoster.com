09109884a9f5cbff45a2f4d63d6a01453cc6bed1
Jeremy Foster <jeremy.foster@live.com>
2016-12-17 22:54:09 -0800

    generate hexo after npm install

--- deploy.sh
+++ deploy.sh
@@ -116,6 +116,10 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
   eval $NPM_CMD install --production
   exitWithMessageOnError "npm failed"
+
+  echo "Generating hexo site..."
+  ./node_modules/hexo/bin/hexo generate
+
   cd - > /dev/null
 fi
 
