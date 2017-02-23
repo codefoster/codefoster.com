1609fcce304018f321281823c7a59e76ac28c2ba
Jeremy Foster <jeremy.foster@live.com>
2017-02-22 20:38:58 -0500

    remove root level package.json and server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var dir = path.join(__dirname,'public');
-var port = process.env.PORT || 3000;
-express()
-    .use(express.static(dir))
-    .listen(port);
-console.log('Serving ' + dir + ' on port ' + port);
